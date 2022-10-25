01f2ba19098269989d6cd62594278f541b796ee4
Pericles <113788303+OPericao@users.noreply.github.com>
2022-10-25 12:19:41 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int diaSemana();
+void imprimirDia();
+
+int main()
+{
+    imprimirDia(diaSemana());
+}
+
+int diaSemana()
+{
+    char inicial,*dia;
+    do
+    {
+        printf("Introduzca la inicial del dia de la semana:");
+        scanf("%c",&inicial);
+    }while(inicial!='L' && inicial!='M' && inicial!='X' && inicial!='J' &&
+    inicial!='V' && inicial!='S' && inicial!='D');
+    switch(inicial) {
+        case 'L':
+            dia="Lunes";
+            break;
+        case 'M':
+            dia="Martes";
+            break;
+        case 'X':
+            dia="Miercoles";
+            break;
+        case 'J':
+            dia="Jueves";
+            break;
+        case 'V':
+            dia="Viernes";
+            break;
+        case 'S':
+            dia="Sabado";
+            break;
+        case 'D':
+            dia="Domingo";
+            break;
+    }
+    return dia;
+}
+
+void imprimirDia(dia)
+{
+    printf("%s",dia);
+}
