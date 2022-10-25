e6a007e964c326cc24b1126a5a5a58ae70f0d0a7
Pericles <113788303+OPericao@users.noreply.github.com>
2022-10-25 12:22:17 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("1");
+    printf("\n2\t3");
+    printf("\n4\t5\t6");
+    printf("\n7\t8\t9\t10");
+    printf("\n11\t12\t13\t14\t15");
+    printf("\n16\t17\t18\t19\t20\t21");
+    return 0;
+}
